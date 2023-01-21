a6ce1033d8db74d875779fab26d630aba063806a
mmattel <github@diemattels.at>
2023-01-21 11:26:14 +0100

    update

--- scripts/syslog_mqtt/syslog_construct_update.py
+++ scripts/syslog_mqtt/syslog_construct_update.py
@@ -1,38 +1,21 @@
 import json
 
+ #   message[0] # timestamp "%Y.%m.%d"
+ #   message[1] # timestamp "%H:%M"
+ #   message[2] # short
+ #   message[3] # severity
+ #   message[4] # set the uptime
+ #   message[5] # set the message
+ 
 def construct_update_message(message=''):
 
     construct = {
-        'date':
-                {'name': 'Date',
-                 'icon': 'calendar',
-                 'sensor_type': 'sensor',
-                 'value_template': message[1]},
-        'time':
-                {'name': 'Time',
-                 'icon': 'calendar-clock',
-                 'sensor_type': 'sensor',
-                 'value_template': message[2]},
-        'host':
-                {'name': 'Host',
-                 'icon': 'server',
-                 'sensor_type': 'sensor',
-                 'value_template': message[3]},
-        'severity':
-                {'name': 'Severity',
-                 'icon': 'alert-circle',
-                 'sensor_type': 'sensor',
-                 'value_template': message[4]},
-        'uptime':
-                {'name': 'Up Time',
-                 'icon': 'arrow-up-bold',
-                 'sensor_type': 'sensor',
-                 'value_template': message[5]},
-        'message':
-                {'name': 'Message',
-                 'icon': 'file-document-outline',
-                 'sensor_type': 'sensor',
-                 'value_template': message[6]},
+        'date': message[1],
+        'time': message[2],
+        'host': message[3],
+        'severity': message[4],
+        'uptime': message[5],
+        'message': message[6],
     }
 
     final = json.dumps(construct)
