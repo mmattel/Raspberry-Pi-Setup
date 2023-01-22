e63098d14658ed03361718cdae2f1c3d1e77a043
mmattel <github@diemattels.at>
2023-01-22 12:51:37 +0100

    update

--- scripts/syslog_mqtt/syslog_filter.py
+++ scripts/syslog_mqtt/syslog_filter.py
@@ -8,15 +8,27 @@
 # reject all messages which are not of interest (return 0)
 def filter_syslog_message(message=''):
 
-    if message[2] == 'kern.uptime.filer':       # regular update message 1x per h
+    if message[2] == 'kern.syslogd.restarted':      # syslog daemon has been restarted due to syslog.conf changes
         return 0
-    if message[2] == 'asup.smtp.drop':          # autosupport mail was not sent for host
+    if message[2] == 'kern.uptime.filer':           # regular update message 1x per h
         return 0
-    if message[2] == 'asup.general.reminder':   # autosupport is not configured to send to NetApp
+    if message[2] == 'asup.smtp.drop':              # autosupport mail was not sent for host
         return 0
-    if message[2] == 'kern.syslogd.restarted':  # syslog daemon has been restarted due to syslog.conf changes
+    if message[2] == 'asup.general.reminder':       # autosupport is not configured to send to NetApp
         return 0
-    if message[2] == 'asup.smtp.detailNotSent': # no autosupport.to recipients specified
+    if message[2] == 'asup.smtp.detailNotSent':     # no autosupport.to recipients specified
+        return 0
+    if message[2] == 'raid.rg.scrub.suspended':     # scrub suspended at stripe
+        return 0
+    if message[2] == 'raid.rg.scrub.resume':        # resuming scrub at stripe
+        return 0
+    if message[2] == 'raid.scrub.suspended':        # disk scrub suspended
+        return 0
+    if message[2] == 'kern.log.rotate':             # system filer is running netApp release
+        return 0
+    if message[2] == 'wafl.scan.start':             # starting block reallocation on aggregate
+        return 0
+    if message[2] == 'wafl.scan.br.realloc.done':   # block reallocation scan on aggregate
         return 0
 
     return True
