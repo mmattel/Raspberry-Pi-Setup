579cc39a577d6ebe15d1b52d2147575cc6139539
mmattel <github@diemattels.at>
2023-01-30 08:37:46 +0100

    update

--- scripts/syslog_mqtt/syslog_filter.py
+++ scripts/syslog_mqtt/syslog_filter.py
@@ -1,6 +1,6 @@
 #   message[0] # timestamp "%Y.%m.%d"
 #   message[1] # timestamp "%H:%M"
-#   message[2] # short
+#   message[2] # type
 #   message[3] # severity
 #   message[4] # set the uptime
 #   message[5] # set the message
@@ -18,8 +18,8 @@ def filter_syslog_message(message=''):
         return 0
     if message[2] == 'cifs.terminationNotice':      # cifs shut down completed, cifs terminated
         return 0
-#    if message[2] == 'kern.syslogd.restarted':      # syslog daemon has been restarted due to syslog.conf changes
-#        return 0
+    if message[2] == 'kern.syslogd.restarted':      # syslog daemon has been restarted due to syslog.conf changes
+        return 0
     if message[2] == 'kern.uptime.filer':           # regular update message 1x per h
         return 0
     if message[2] == 'kern.log.rotate':             # system filer is running netApp release
@@ -27,6 +27,10 @@ def filter_syslog_message(message=''):
     if message[2] == 'mgr.boot.disk_done':          # boot message
         return 0
     if message[2] == 'nbt.nbns.registrationComplete':  # all cifs name registrations have completed for the local server
+        return 0
+    if message[2] == 'raid.rg.scrub.done':          # scrub completed
+        return 0
+    if message[2] == 'raid.rg.scrub.summary.cksum': # scrub found 0 checksum errors
         return 0
     if message[2] == 'raid.rg.scrub.suspended':     # scrub suspended at stripe
         return 0
@@ -43,6 +47,8 @@ def filter_syslog_message(message=''):
     if message[2] == 'wafl.scan.start':             # starting block reallocation on aggregate
         return 0
     if message[2] == 'wafl.scan.br.realloc.done':   # block reallocation scan on aggregate
+        return 0
+    if message[2] == 'wafl.scan.br.redir.done':     # redirect scan on aggregate is complete
         return 0
 
     return True
