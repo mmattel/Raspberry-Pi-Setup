e1140fac7953639e6071bffbd8d8813d748ccd16
mmattel <github@diemattels.at>
2024-02-18 21:10:17 +0100

    update

--- scripts/syslog_mqtt/syslog_filter.py
+++ scripts/syslog_mqtt/syslog_filter.py
@@ -2,49 +2,49 @@
 
 def filter_syslog_message(message=''):
 
-    if message == 'asup.smtp.drop':              # autosupport mail was not sent for host
+    if message in 'asup.smtp.drop':              # autosupport mail was not sent for host
         return 0
-    if message == 'asup.smtp.sent':              # system Notification mail sent
+    if message in 'asup.smtp.sent':              # system Notification mail sent
         return 0
-    if message == 'asup.general.reminder':       # autosupport is not configured to send to NetApp
+    if message in 'asup.general.reminder':       # autosupport is not configured to send to NetApp
         return 0
-    if message == 'asup.smtp.detailNotSent':     # no autosupport.to recipients specified
+    if message in 'asup.smtp.detailNotSent':     # no autosupport.to recipients specified
         return 0
-    if message == 'cecc_log.entry:warning':      # Non Fatal Correctable DRAM ECC Channel x
+    if message in 'cecc_log.entry:warning':      # Non Fatal Correctable DRAM ECC Channel x
         return 0
-    if message == 'cifs.terminationNotice':      # cifs shut down completed, cifs terminated
+    if message in 'cifs.terminationNotice':      # cifs shut down completed, cifs terminated
         return 0
-    if message == 'kern.syslogd.restarted':      # syslog daemon has been restarted due to syslog.conf changes
+    if message in 'kern.syslogd.restarted':      # syslog daemon has been restarted due to syslog.conf changes
         return 0
-    if message == 'kern.uptime.filer':           # regular update message 1x per h
+    if message in 'kern.uptime.filer':           # regular update message 1x per h
         return 0
-    if message == 'kern.log.rotate':             # system filer is running netApp release
+    if message in 'kern.log.rotate':             # system filer is running netApp release
         return 0
-    if message == 'mgr.boot.disk_done':          # boot message
+    if message in 'mgr.boot.disk_done':          # boot message
         return 0
-    if message == 'nbt.nbns.registrationComplete':  # all cifs name registrations have completed for the local server
+    if message in 'nbt.nbns.registrationComplete':  # all cifs name registrations have completed for the local server
         return 0
-    if message == 'raid.rg.scrub.done':          # scrub completed
+    if message in 'raid.rg.scrub.done':          # scrub completed
         return 0
-    if message == 'raid.rg.scrub.summary.cksum': # scrub found 0 checksum errors
+    if message in 'raid.rg.scrub.summary.cksum': # scrub found 0 checksum errors
         return 0
-    if message == 'raid.rg.scrub.suspended':     # scrub suspended at stripe
+    if message in 'raid.rg.scrub.suspended':     # scrub suspended at stripe
         return 0
-    if message == 'raid.rg.scrub.resume':        # resuming scrub at stripe
+    if message in 'raid.rg.scrub.resume':        # resuming scrub at stripe
         return 0
-    if message == 'raid.scrub.suspended':        # disk scrub suspended
+    if message in 'raid.scrub.suspended':        # disk scrub suspended
         return 0
-    if message == 'rc':                          # timed: time daemon started
+    if message in 'rc':                          # timed: time daemon started
         return 0
-    if message == 'ses.channel.rescanInitiated': # Initiating rescan on channel
+    if message in 'ses.channel.rescanInitiated': # Initiating rescan on channel
         return 0
-    if message == 'sfu.firmwareUpToDate':        # firmware is up-to-date on all disk shelves
+    if message in 'sfu.firmwareUpToDate':        # firmware is up-to-date on all disk shelves
         return 0
-    if message == 'wafl.scan.start':             # starting block reallocation on aggregate
+    if message in 'wafl.scan.start':             # starting block reallocation on aggregate
         return 0
-    if message == 'wafl.scan.br.realloc.done':   # block reallocation scan on aggregate
+    if message in 'wafl.scan.br.realloc.done':   # block reallocation scan on aggregate
         return 0
-    if message == 'wafl.scan.br.redir.done':     # redirect scan on aggregate is complete
+    if message in 'wafl.scan.br.redir.done':     # redirect scan on aggregate is complete
         return 0
 
     return True
