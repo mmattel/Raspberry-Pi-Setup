f926da14e8dd1f023d79576420859f00c5b895ad
Martin Mattel <github@diemattels.at>
2024-10-19 18:19:10 +0200

    Make syslog_mqtt to use the new paho API

--- scripts/syslog_mqtt/syslog_mqtt.py
+++ scripts/syslog_mqtt/syslog_mqtt.py
@@ -3,45 +3,92 @@ import sys
 import signal
 import socket
 import syslog
-import datetime
+import time
 import paho.mqtt.client as mqtt
+from paho.mqtt.packettypes import PacketTypes
 from dotenv import dotenv_values
 import syslog_filter as sf              # python syslog filter for filer messages
 import syslog_regex as sr               # python syslog parser for filer messages
 import syslog_construct_update as scu   # construct update from message response
 import syslog_construct_ha as sch       # construct ha from message response
 
-# script and pip needs to be run as root due to socket access 
-# pip install paho-mqtt
+# script and pip needs to be run as root due to socket access
+# sudo python -m pip install --upgrade pip
+# sudo python -m pip install paho-mqtt
+# sudo python -m pip install python-dotenv
 
 # main program
 
 # must run as root
 if os.geteuid() != 0:
-    print("You need to run the program with root previleges like with sudo.")
-    sys.exit()
+	print("You need to run the program with root previleges like with sudo.")
+	sys.exit()
 
 def graceful_shutdown():
-    print()
-    # the will_set is not sent on graceful shutdown by design
-    # we need to wait until the message has been sent, else it will not appear in the broker
-    publish_result = mqttclient.publish(mqtt_state_topic, payload = "offline", qos = mqtt_qos, retain = True)
-    publish_result.wait_for_publish() 
-    mqttclient.disconnect()
-    mqttclient.loop_stop()
-    s.close()
-    sys.exit()
+	global mqttclient
+	global connect_ok
+	print()
+	# the will_set is not sent on graceful shutdown by design
+	# we need to wait until the message has been sent, else it will not appear in the broker
+	if connect_ok:
+		try:
+			publish_result = mqttclient.publish(
+				mqtt_availability_topic,
+				payload = 'offline',
+				qos = mqtt_qos,
+				retain = True,
+				properties = None
+			)
+    		# max wait 4 sec to get the message published.
+			# there can be cases where this would run forever
+			publish_result.wait_for_publish(4) 
+			mqttclient.disconnect()
+			mqttclient.loop_stop()
+		except Exception:
+				pass
+	connect_ok = False
+	s.close()
+	sys.exit()
 
 # catch ctrl-c
 def signal_handler(signum, frame):
-    graceful_shutdown()
+	graceful_shutdown()
 
-def on_connect(client, userdata, flags, rc):
-    # http://www.steves-internet-guide.com/mqtt-python-callbacks/
-    client.publish(mqtt_state_topic, payload = "Online", qos = 0, retain = True)
+def on_connect(client, userdata, flags, reason_code, properties = None):
+	# http://www.steves-internet-guide.com/mqtt-python-callbacks/
+	# online/offline needs to be exactly written like that for proper recognition in HA
+	global connect_ok
+	if reason_code == 0:
+		client.publish(
+			mqtt_availability_topic,
+			payload = 'online',
+			qos = mqtt_qos,
+			retain = True
+		)
+		connect_ok = True
+	else:
+		connect_ok = False
 
-def on_publish(client, userdata, mid):
-    print(f"Messages published: {mid}")
+def on_disconnect(client, userdata, disconnect_flags, reason_code, properties = None):
+	# if we have not been connected formerly, it makes no sense to tell that we are now disconnected
+	global connect_ok
+	if not connect_ok:
+		return
+
+	if reason_code != 0:
+		# if it has been connected once sucessfully, it tries to reconnect automatically
+		# means no manual reconnect/raise/try/error is necessary
+
+		# https://github.com/eclipse/paho.mqtt.python/blob/master/src/paho/mqtt/reasoncodes.py
+		# https://github.com/eclipse/paho.mqtt.python/issues/827
+		syslog.syslog('MQTT: Disconnected: '
+			+ str(reason_code.getName())
+			+ ', packet: '
+			+ str(PacketTypes.Names[reason_code.packetType])
+			)
+
+def on_publish(client, userdata, message, reason_codes, properties = None):
+	print(f"MQTT messages published: {message}")
 
 def process_env():
 	# get all environment variables as dictionary
@@ -69,21 +116,26 @@ mqtt_config = process_env()
 # when sending messages to syslog, they appear in /var/log/messages
 
 # MQTT Parameters
-# when running an a container, values can be overwritten by env vars
+# use either envvars or the .env file, envvars overwrite .env
 mqtt_server = mqtt_config['mqtt_server']
 mqtt_port = int(mqtt_config['mqtt_port'])
 mqtt_username = mqtt_config['mqtt_username']
 mqtt_password = mqtt_config['mqtt_password']
 mqtt_client_id = mqtt_config['mqtt_client_id']
 mqtt_ha_topic = 'homeassistant/sensor/' + mqtt_config['mqtt_topic']
-mqtt_update_topic = 'syslog/sensor/' + mqtt_config['mqtt_topic']
-mqtt_state_topic = 'syslog/sensor/' + mqtt_config['mqtt_topic'] + '/availability'
+mqtt_state_topic = 'syslog/sensor/' + mqtt_config['mqtt_topic']
+mqtt_availability_topic = 'syslog/sensor/' + mqtt_config['mqtt_topic'] + '/availability'
 
 # MQTT qos values (http://www.steves-internet-guide.com/understanding-mqtt-qos-levels-part-1/)
 # QOS 0 – Once (not guaranteed)
 # QOS 1 – At Least Once (guaranteed)
 # QOS 2 – Only Once (guaranteed)
 mqtt_qos = 2
+
+# protocol versions available
+# MQTTv31  = 3
+# MQTTv311 = 4
+# MQTTv5   = 5
 
 # Syslog Parameters
 # Insert IP of server listener. 0.0.0.0 for any
@@ -92,6 +144,8 @@ port = 514
 buf = 4096
 addr = (server,port)
 
+connect_ok = None
+
 # Open Socket for syslog messages
 # https://realpython.com/python-sockets/
 syslog.syslog(f'Opening syslog socket: {server}:{port}')
@@ -99,22 +153,48 @@ s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(addr)
 
 if s.bind:
-    syslog.syslog(f'Opened syslog socket: {server}:{port}')
+	syslog.syslog(f'Opened syslog socket: {server}:{port}')
 else:
-    syslog.syslog(f'Could not open syslog socket: {server}:{port}. Exiting')
-    sys.exit()
+	syslog.syslog(f'Could not open syslog socket: {server}:{port}. Exiting')
+	sys.exit()
 
 # MQTT client
 syslog.syslog(f'Opening MQTT socket: {mqtt_server}:{mqtt_port}')
 
-mqttclient = mqtt.Client(client_id = mqtt_client_id, clean_session = True)
+mqttclient = mqtt.Client(
+				mqtt.CallbackAPIVersion.VERSION2,
+				protocol = mqtt.MQTTv5,
+				client_id = mqtt_client_id
+			)
 mqttclient.on_connect = on_connect
-# mqttclient.on_publish = on_publish      # uncomment for testing purposes    
+mqttclient.on_disconnect = on_disconnect
 mqttclient.username_pw_set(mqtt_username, mqtt_password)
-mqttclient.will_set(mqtt_state_topic, payload = "Offline", qos = 0, retain = True)
-
-mqttclient.connect_async(mqtt_server, port=mqtt_port, keepalive=70)
+mqttclient.will_set(
+				mqtt_availability_topic,
+				payload = 'offline',
+				qos = 0,
+				retain = True
+			)
+mqttclient.connect(
+				mqtt_server,
+				port = mqtt_port,
+				# http://www.steves-internet-guide.com/mqtt-keep-alive-by-example/
+				# no need to set this value with paho-mqtt
+				# this avoids on the broker the following message pairs beling logged
+				# "Client solmate_mqtt closed its connection.
+				# "Client xyz closed its connection.
+				#keepalive = 70,
+				bind_address = '',
+				bind_port = 0,
+				clean_start = mqtt.MQTT_CLEAN_START_FIRST_ONLY,
+				properties = None
+			)
 mqttclient.loop_start()
+
+# wait until on_connect returns a response
+while connect_ok == None:
+	# wait until the connection is either established or failed (like user/pwd typo)
+	time.sleep(1)
 
 if mqttclient:
     syslog.syslog(f'Opened MQTT socket: {mqtt_server}:{mqtt_port}')
@@ -126,13 +206,21 @@ else:
 # each item needs its own publish
 # name and config are arrays
 # name contains the name for the config which is the full json string defining the message
-name, config = sch.construct_ha_message(mqtt_config['mqtt_topic'], mqtt_state_topic, mqtt_update_topic)
+name, config = sch.construct_ha_message(mqtt_config['mqtt_topic'], mqtt_availability_topic, mqtt_state_topic)
 
 for i in range(0,len(name)): 
-    ha_topic_construct = mqtt_ha_topic + '/' + name[i] + '/config'
-    # print(str(i) + ' ' + ha_topic_construct)
-    # print(config[i])
-    mqttclient.publish(ha_topic_construct, payload = config[i], qos = mqtt_qos, retain = True)
+	ha_topic_construct = mqtt_ha_topic + '/' + name[i] + '/config'
+	#print(str(i) + ' ' + ha_topic_construct)
+	#print(config[i])
+	mqttclient.publish(
+			ha_topic_construct,
+			payload = config[i],
+			qos = mqtt_qos,
+			retain = True,
+			properties = None
+		)
+
+#graceful_shutdown()
 
 while True:
     # sudo nc -ulp 514  (read messages from command line)
@@ -140,29 +228,35 @@ while True:
     # data contains the message in utf-8
     # sender is a tuple with (string)IP (int)port
     # socket.gethostbyaddr(sender[0]) returns a triple ('hostname', [], ['IP'])
-    data,sender = s.recvfrom(buf)
-    if not data:
-        syslog.syslog(f'Cant recieve data from socket {server}:{port}. Exiting')
-        break
-    else:
-        # only continue if the message comes from filer
-        if mqtt_config['mqtt_topic'] not in socket.gethostbyaddr(sender[0])[0]:
-            continue
+	data,sender = s.recvfrom(buf)
+	if not data:
+		syslog.syslog(f'Cant recieve data from socket {server}:{port}. Exiting')
+		break
+	else:
+		# only continue if the message comes from filer
+		if mqtt_config['mqtt_topic'] not in socket.gethostbyaddr(sender[0])[0]:
+			continue
 
-        # decode the complete message string from syslog
-        msg_string = data.decode(encoding='utf-8')
+		# decode the complete message string from syslog
+		msg_string = data.decode(encoding='utf-8')
 
-        # only proceed if response is important (False == unimportant)
-        if not sf.filter_syslog_message(msg_string):
-            continue
+		# only proceed if response is important (False == unimportant)
+		if not sf.filter_syslog_message(msg_string):
+			continue
 
-        # setup response from data
-        response = sr.parse_syslog_message(msg_string)
-        #print(response)
+		# setup response from data
+		response = sr.parse_syslog_message(msg_string)
+		#print(response)
 
-        update = scu.construct_update_message(response)
-        # send a mqtt update message, the format is fixed
-        mqttclient.publish(mqtt_update_topic, payload = update, qos = mqtt_qos, retain = True)
+		update = scu.construct_update_message(response)
+		# send a mqtt update message, the format is fixed
+		mqttclient.publish(
+				mqtt_state_topic,
+				payload = update,
+				qos = mqtt_qos,
+				retain = True,
+				properties = None
+			)
 
 graceful_shutdown()
 # as reminder
